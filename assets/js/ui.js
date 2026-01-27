344fcec5c1f2e345ea8da14f3699ffa43c0705c1
Jungler <33758831+Reibik@users.noreply.github.com>
2026-01-28 02:19:14 +0300

    Update ui.js

--- assets/js/ui.js
+++ assets/js/ui.js
@@ -1,3 +1,11 @@
+function openModal() { document.getElementById('rulesModal').style.display = 'block'; }
+function closeModal() { document.getElementById('rulesModal').style.display = 'none'; }
+// Закрыть при клике вне окна
+window.onclick = function(event) {
+    if (event.target == document.getElementById('rulesModal')) {
+        closeModal();
+    }
+}
 document.querySelectorAll('.nav a').forEach(link => {
     if (link.href === window.location.href) {
         link.classList.add('active');
